4b5cbc0c6576a5cce76add4acb56432f71aadf95
Ross Fleming <rfleming@junipercommerce.com>
2022-06-09 16:09:49 -0400

    Create domain objects class

---
+++ TaxService.Domain/Order.cs
@@ -0,0 +1,13 @@
+﻿namespace TaxService.Domain;
+
+// TODO: Validation
+// TODO: What can and can't be null?
+
+public class Order
+{
+    public string? ToCountry { get; set; }
+    public string? ToZip { get; set; }
+    public string? ToState { get; set; }
+    public decimal OrderAmount { get; set; }
+    public decimal ShippingAmount { get; set; }
+}
